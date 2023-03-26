20bb4028d82730597418e34b712c3235ac0d2030
aka-lowe <93315804+aka-lowe@users.noreply.github.com>
2023-03-26 13:15:57 +0200

    Merge akira

--- src/main/java/Base/Player.java
+++ src/main/java/Base/Player.java
@@ -20,6 +20,17 @@ public class Player {
     }
 
     private void displayShotBoard() {
+        System.out.println("+---------------------+");
+        System.out.println("  0 1 2 3 4 5 6 7 8 9 ");
+        for (int i = 0; i < 10; i++) {
+            System.out.print("| ");
+            for (int j = 0; j < 10; j++) {
+                System.out.print( shotResults[j][i] + " ");
+            }
+            System.out.println(" |");
+        }
+        System.out.println("  0 1 2 3 4 5 6 7 8 9 ");
+        System.out.println("+---------------------+");
 
     }
 
