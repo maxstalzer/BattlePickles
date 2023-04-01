daf06c1351b39931c6f392ddf21e55bf9fc9aba0
aka-lowe <93315804+aka-lowe@users.noreply.github.com>
2023-04-01 17:21:35 +0200

    updated cucumber tests

--- src/test/java/StepsDefinition.java
+++ src/test/java/StepsDefinition.java
@@ -9,6 +9,10 @@ public class StepsDefinition {
     Tile t;
     Gurkin g;
 
+    Player p1;
+    Board p2b;
+    Player p2;
+
     @Given("a tile")
     public void a_tile() {
         t = new Tile();
@@ -23,4 +27,26 @@ public class StepsDefinition {
     public void the_gurkin_is_on_the_tile() {
         assertEquals(g, t.getGurkin());
     }
+
+    @Given("a player")
+    public void a_player() {
+        p1 = new Player("Akira");
+    }
+    @Given("a board")
+    public void a_board() {
+       p2b = new Board();
+    }
+    @When("I shoot a tile with a gurkin on it")
+    public void i_shoot_a_tile_with_a_gurkin_on_it() {
+        g = new Gherkin();
+        p2b.getTile(new Coordinates(0, 0)).setGurkin(g);
+    }
+    @Then("I get the string hit")
+    public void i_get_the_string_hit() {
+
+        assertEquals("hit", p2b.attack(new Coordinates(0, 0)));
+    }
+
+
+
 }
--- src/test/resources/featureFiles/PlayerCreation.feature
+++ src/test/resources/featureFiles/PlayerCreation.feature
@@ -5,3 +5,9 @@ Feature: Player Creation
     Given a tile
     When set a gurkin on the tile
     Then the gurkin is on the tile
+  @tag1
+  Scenario: shooting a tile with a gurkin on
+    Given a player
+    And a board
+    When I shoot a tile with a gurkin on it
+    Then I get the string hit
