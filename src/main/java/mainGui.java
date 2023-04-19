9e28aa2a0ea337d2d389e8bdb292ab8297a6c8f0
The-Laug <54498065+The-Laug@users.noreply.github.com>
2023-04-19 10:54:07 +0200

    Working

--- src/main/java/mainGui.java
+++ src/main/java/mainGui.java
@@ -1,6 +1,7 @@
 import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.layout.GridPane;
+import javafx.scene.paint.Color;
 import javafx.scene.shape.Rectangle;
 import javafx.stage.Stage;
 
@@ -8,19 +9,17 @@ public class mainGui extends Application {
     int size = 10;
     @Override
     public void start(Stage primaryStage) throws Exception {
-        primaryStage.setTitle("First GUI window!");
         GridPane grid = new GridPane();
-        for (int i =0; i<size; i++) {
-            for (int j=0; i<size; j++) {
-                Rectangle r = new Rectangle(100,100,100,100);
-                grid.add(r, i, j);
+        double s = 100; // side of rectangle
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < size; j++) {
+                Rectangle r = new Rectangle(s, s, s, s);
+                grid.add(r, j, i);
             }
         }
 
 
-
-
-
+        primaryStage.setTitle("Battleboard");
         Scene scene = new Scene(grid);
         primaryStage.setScene(scene);
         primaryStage.show();
