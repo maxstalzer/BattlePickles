615955e303fbdbce12855632e3fc498e4c121935
Your Name <tb2r@icloud.com>
2023-04-27 14:11:20 +0200

    More cosmetic updates on the GameView

--- src/main/java/Gui/SidePanel.java
+++ src/main/java/Gui/SidePanel.java
@@ -48,24 +48,39 @@ public class SidePanel extends VBox implements PlayerObserver {
     Controller controller;
     public SidePanel(Controller controller) {
         this.controller=controller;
-        javafx.scene.control.Label label1 = new javafx.scene.control.Label("Choose Gurk Type ©");
+        javafx.scene.control.Label label1 = new javafx.scene.control.Label("Select Gurk Direction  ©");
+        label1.setStyle(
+                "-fx-font-family: Joystix ; " +
+                        "-fx-font-size: 18;" +
+                        "-fx-border-color: transparent, black;");
         setAlignment(Pos.CENTER);
 
         Button Quit = new Button("Quit");
+        Quit.setStyle( "-fx-font-family: Joystix ; -fx-font-size: 18;");
         Quit.setOnAction(e -> controller.showMainMenu());
         getChildren().add(Quit);
 
 
 
+
         getChildren().add(label1);
-        MenuButton menu = new MenuButton("Directions");
-        MenuItem horiz = new MenuItem("Horizontal");
+
+        MenuButton menu = new MenuButton("");
+        menu.setStyle( "-fx-font-family: Joystix ; -fx-font-size: 18;");
+        MenuItem horiz = new MenuItem("");
+        horiz.setStyle( "-fx-font-family: Joystix ; -fx-font-size: 18;");
         MenuItem vert = new MenuItem("Vertical");
+        vert.setStyle( "-fx-font-family: Joystix ; -fx-font-size: 18;");
+        menu.setText("Horizontal");
+//see if we would like to use this as it is
+        menu.getItems().addAll( horiz, vert);
+        getChildren().add(menu);
+        //menu.getItems().forEach(menuItem -> menuItem.setOnAction(event -> {
+        //   menu.setText(menuItem.getText());
+        //}));
+
         horiz.setOnAction(e -> setDir(Horizontal));
         vert.setOnAction(e -> setDir(Vertical));
-
-        menu.getItems().addAll(horiz,vert);
-        getChildren().add(menu);
 
 
 
